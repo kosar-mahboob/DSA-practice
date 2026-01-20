aa5a907395eac76d9056c83a0722d980701b9f47
Kosar Bibi <mahbooburrehman001@gmail.com>
2026-01-20 23:01:50 +0500

    Create Construct the Minimum Bitwise Array I.py

---
+++ BitManipulation/Construct the Minimum Bitwise Array I.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def minBitwiseArray(self, nums: List[int]) -> List[int]:
+        ans = []
+        for n in nums:
+            val = -1
+            for x in range(n):
+                if x | (x + 1) == n:
+                    val = x
+                    break
+            ans.append(val)
+        return ans
