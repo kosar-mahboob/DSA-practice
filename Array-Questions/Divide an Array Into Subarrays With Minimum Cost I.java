028c896830a54657ede1f6c01bd59f72bab08de7
Kosar Bibi <mahbooburrehman001@gmail.com>
2026-02-01 19:16:57 +0500

    Create Divide an Array Into Subarrays With Minimum Cost I.java

---
+++ Array-Questions/Divide an Array Into Subarrays With Minimum Cost I.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minimumCost(int[] nums) {
+        int n = nums.length;
+        
+        // Find the two smallest values in nums[1..n-1]
+        int firstMin = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
+        
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < firstMin) {
+                secondMin = firstMin;
+                firstMin = nums[i];
+            } else if (nums[i] < secondMin) {
+                secondMin = nums[i];
+            }
+        }
+        
+        return nums[0] + firstMin + secondMin;
+    }
+}
