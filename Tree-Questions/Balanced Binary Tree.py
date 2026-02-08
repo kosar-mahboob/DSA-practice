77b8955dcd6925b721f4d5529c9e44b31860b532
Kosar Bibi <mahbooburrehman001@gmail.com>
2026-02-08 18:59:17 +0500

    Implement balanced binary tree checker

---
+++ Tree-Questions/Balanced Binary Tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root):
+        def checkHeight(node):
+            if not node:
+                return 0  # Height of empty tree
+            
+            leftHeight = checkHeight(node.left)
+            if leftHeight == -1:
+                return -1
+            
+            rightHeight = checkHeight(node.right)
+            if rightHeight == -1:
+                return -1
+            
+            if abs(leftHeight - rightHeight) > 1:
+                return -1
+            
+            return max(leftHeight, rightHeight) + 1
+        
+        return checkHeight(root) != -1
