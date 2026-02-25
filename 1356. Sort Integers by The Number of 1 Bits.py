14296eed8ba5d48c37d150a85e76640bd06c5ec2
Kosar Bibi <mahbooburrehman001@gmail.com>
2026-02-25 21:58:19 +0500

    Add sorting function by number of 1 bits
    Implement sorting of integers by the number of 1 bits and value.

---
+++ 1356. Sort Integers by The Number of 1 Bits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        """
+        Sorts the array by the number of 1 bits in the binary representation,
+        then by the integer value.
+        """
+        # Use a tuple key: (bit_count, value)
+        arr.sort(key=lambda x: (x.bit_count(), x))
+        return arr
