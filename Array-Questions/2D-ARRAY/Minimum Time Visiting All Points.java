6e07f79ab9192a39e56134954e3b4ff3cf9aead9
Kosar Bibi <mahbooburrehman001@gmail.com>
2026-03-08 23:53:29 +0500

    Fix formatting and add missing code in solution

--- Array-Questions/2D-ARRAY/Minimum Time Visiting All Points.java
+++ Array-Questions/2D-ARRAY/Minimum Time Visiting All Points.java
@@ -2,7 +2,7 @@
 class Solution {
     public int minTimeToVisitAllPoints(int[][] points) {
         int totalTime = 0;
-        
+    
         for (int i = 1; i < points.length; i++) {
             totalTime += Math.max(
                 Math.abs(points[i][0] - points[i - 1][0]),
