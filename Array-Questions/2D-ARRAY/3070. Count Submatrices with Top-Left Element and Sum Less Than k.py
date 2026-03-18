c8336e992dbd68f4e107eb9a64b312ace96f4de3
Kosar Bibi <mahbooburrehman001@gmail.com>
2026-03-18 23:25:26 +0500

    Add method to count submatrices with sum less than k
    Implement a method to count submatrices with a top-left element and a sum less than k using prefix sums.

---
+++ Array-Questions/2D-ARRAY/3070. Count Submatrices with Top-Left Element and Sum Less Than k.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countSubmatrices(self, grid: List[List[int]], k: int) -> int:
+        m, n = len(grid), len(grid[0])
+        # Compute prefix sums in place
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    continue
+                elif i == 0:
+                    grid[i][j] += grid[i][j-1]
+                elif j == 0:
+                    grid[i][j] += grid[i-1][j]
+                else:
+                    grid[i][j] += grid[i-1][j] + grid[i][j-1] - grid[i-1][j-1]
+
+        # Count valid submatrices
+        count = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] <= k:
+                    count += 1
+        return count
