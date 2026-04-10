535630dea6ab6014989ca51ff80ff6fa2f4e7dfc
Kosar Bibi <mahbooburrehman001@gmail.com>
2026-04-10 23:06:25 +0500

    Clean up code by removing blank line
    Remove unnecessary blank line in the function.

--- Array-Questions/1536. Minimum Swaps to Arrange a Binary Grid.py
+++ Array-Questions/1536. Minimum Swaps to Arrange a Binary Grid.py
@@ -6,7 +6,6 @@ class Solution:
         for row in grid:
             # scan from right to left
             rightmost = -1
-            
             for j in range(n-1, -1, -1):
                 if row[j] == 1:
                     rightmost = j
