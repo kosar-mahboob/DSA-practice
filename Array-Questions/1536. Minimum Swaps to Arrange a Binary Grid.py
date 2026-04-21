f2ffaf4fc0bee22557b65e3e36f7d1140b2dbb95
Kosar Bibi <mahbooburrehman001@gmail.com>
2026-04-21 22:34:04 +0500

    Update 1536. Minimum Swaps to Arrange a Binary Grid.py

--- Array-Questions/1536. Minimum Swaps to Arrange a Binary Grid.py
+++ Array-Questions/1536. Minimum Swaps to Arrange a Binary Grid.py
@@ -6,6 +6,7 @@ class Solution:
         for row in grid:
             # scan from right to left
             rightmost = -1
+            
             for j in range(n-1, -1, -1):
                 if row[j] == 1:
                     rightmost = j
