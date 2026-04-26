3c73fcd380fa50ef148a9448f69c8b07349cfcf2
Kosar Bibi <mahbooburrehman001@gmail.com>
2026-04-26 23:32:10 +0500

    Implement cycle detection in 2D grid

---
+++ Array-Questions/1559. Detect Cycles in 2D Grid.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def containsCycle(self, grid: List[List[str]]) -> bool:
+        m, n = len(grid), len(grid[0])
+        visited = [[False] * n for _ in range(m)]
+        dirs = [(1,0), (-1,0), (0,1), (0,-1)]
+        
+        for i in range(m):
+            for j in range(n):
+                if not visited[i][j]:
+                    stack = [(i, j, -1, -1)]
+                    visited[i][j] = True
+                    
+                    while stack:
+                        r, c, pr, pc = stack.pop()
+                        for dr, dc in dirs:
+                            nr, nc = r + dr, c + dc
+                            if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == grid[i][j]:
+                                if (nr, nc) == (pr, pc):
+                                    continue
+                                if visited[nr][nc]:
+                                    return True
+                                visited[nr][nc] = True
+                                stack.append((nr, nc, r, c))
+        return False
