cea63ec8bdd4f82f1f7c2b37d6d96898630b28cc
Kosar Bibi <mahbooburrehman001@gmail.com>
2026-05-04 22:11:23 +0500

    Create 48. Rotate Image.py

---
+++ Array-Questions/2D-ARRAY/48. Rotate Image.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        # 1. Transpose (swap across main diagonal)
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        # 2. Reverse each row (clockwise 90°)
+        for row in matrix:
+            row.reverse()
+        
