58f4eafb287cfd3ee7adbad480e0501275c2d349
Kosar Bibi <mahbooburrehman001@gmail.com>
2026-06-04 21:41:30 +0500

    Implement total waviness calculation for a range

---
+++ June-qestions /3751. Total Waviness of Numbers in Range I.py
@@ -0,0 +1,18 @@
+class Solution:
+    def totalWaviness(self, num1: int, num2: int) -> int:
+        def waviness(x: int) -> int:
+            s = str(x)
+            if len(s) < 3:
+                return 0
+            cnt = 0
+            for i in range(1, len(s) - 1):
+                if s[i] > s[i-1] and s[i] > s[i+1]:
+                    cnt += 1
+                elif s[i] < s[i-1] and s[i] < s[i+1]:
+                    cnt += 1
+            return cnt
+        
+        total = 0
+        for n in range(num1, num2 + 1):
+            total += waviness(n)
+        return total
