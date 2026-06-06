b161474ae5c361b6bab09472146cb84a74271fc3
Kosar Bibi <mahbooburrehman001@gmail.com>
2026-06-06 21:24:37 +0500

    Implement left-right sum difference calculation

---
+++ june-questions/2574. Left and Right Sum Differences.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def leftRighDifference(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        left_sum = [0] * n
+        right_sum = [0] * n
+        for i in range(1, n):
+            left_sum[i] = left_sum[i-1] + nums[i-1]
+        for i in range(n-2, -1, -1):
+            right_sum[i] = right_sum[i+1] + nums[i+1]
+        return [abs(left_sum[i] - right_sum[i]) for i in range(n)]
