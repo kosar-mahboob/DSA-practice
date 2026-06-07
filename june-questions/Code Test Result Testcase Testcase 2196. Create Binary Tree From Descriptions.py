712857296fef06b87b8b990742c8c0be239a6920
Kosar Bibi <mahbooburrehman001@gmail.com>
2026-06-07 21:18:53 +0500

    Create  Code Test Result Testcase Testcase 2196. Create Binary Tree From Descriptions.py

---
+++ june-questions/Code Test Result Testcase Testcase 2196. Create Binary Tree From Descriptions.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from typing import List, Optional
+
+class Solution:
+    def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
+        nodes = {}
+        children = set()
+        for p, c, is_left in descriptions:
+            # create parent node if not exists
+            if p not in nodes:
+                nodes[p] = TreeNode(p)
+            if c not in nodes:
+                nodes[c] = TreeNode(c)
+            # attach child
+            if is_left:
+                nodes[p].left = nodes[c]
+            else:
+                nodes[p].right = nodes[c]
+            children.add(c)
+        # find root: value not in children set
+        for val in nodes:
+            if val not in children:
+                return nodes[val]
+        return None
