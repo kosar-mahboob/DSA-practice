3b912b34980a17193189aa36dbcc084c5135331f
Kosar Bibi <mahbooburrehman001@gmail.com>
2026-06-19 22:19:27 +0500

    Implement function to find the highest altitude

--- june-questions/1732. Find the Highest Altitude.py
+++ june-questions/1732. Find the Highest Altitude.py
@@ -1,1 +1,11 @@
-hf
+from typing import List
+
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        cur = 0
+        max_alt = 0
+        for g in gain:
+            cur += g
+            if cur > max_alt:
+                max_alt = cur
+        return max_alt
